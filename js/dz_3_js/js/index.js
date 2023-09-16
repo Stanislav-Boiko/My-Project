ee3dde5d9e003d3bf1fdfa3614e485d835ff4cc3
stas.boyko.99 <stas.boyko.99@gmail.com>
2023-09-16 18:40:13 +0200

    dz_3_js

--- js/dz_3_js/js/index.js
+++ js/dz_3_js/js/index.js
@@ -1,16 +1,4 @@
-// let userNumber =+prompt('Enter Number');
-
-// for (let i=0; i<userNumber; i++ ) {
-//     if (i % 5 === 0) {
-//         console.log(i);
-//     } else {
-//         console.log("Sorry, no numbers");
-//     }
-    
-// }
-
-
-let userNumber =+ prompt('Enter Number');
+let userNumber = +prompt('Enter Number');
 let noNumber = false;
 
 for (let i = 5; i < userNumber; i += 5) {
