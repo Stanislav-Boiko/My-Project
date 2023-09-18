188f26792620271a8c8433ed52da5197d1395a5f
stas.boyko.99 <stas.boyko.99@gmail.com>
2023-09-18 18:22:55 +0200

    dz_3_js

--- js/dz_3_js/js/index.js
+++ js/dz_3_js/js/index.js
@@ -1,11 +1,9 @@
 let userNumber = +prompt('Enter Number');
-let noNumber = false;
 
-for (let i = 5; i < userNumber; i += 5) {
-    console.log(i);
-    noNumber = true;
-}
-
-if (!noNumber) {
-    console.log("Sorry, no numbers");
+for (let i = 0; i < userNumber; i++) {
+    if (i % 5 == 0) {
+        console.log(i);
+    } else if ( userNumber <= 5 ) {
+        console.log("Sorry, no numbers");
+    }
 }
