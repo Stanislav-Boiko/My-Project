f4c838df25a97d50d222cb47405510400ec3b238
stas.boyko.99 <stas.boyko.99@gmail.com>
2024-01-31 19:16:17 +0100

    HW 2

---
+++ JS_PE-28/HW_2/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const username = "Stanislav";
+const password = "1111"
+
+const userPassword = prompt(`Вітаю ${username}, введіть ваш пароль...`);
+
+const result = password === userPassword;
+console.log(result);
+
+const x = 5;
+const y = 3;
+
+alert(x+y);
+alert(x-y);
+alert(x*y);
+alert(x/y);
